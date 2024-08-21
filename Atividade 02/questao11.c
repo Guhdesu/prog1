0118748ee8300aca473a84d1cb45aeeb8b6d36ee
Gustavo Ferreira <carvalhogustavo444@gmail.com>
2024-08-21 15:22:37 -0300

    Update questao11.c

--- Atividade 02/questao11.c
+++ Atividade 02/questao11.c
@@ -8,11 +8,11 @@ int main() {
 
     if (a + b > c && a + c > b && b + c > a) {
         if (a == b && b == c) {
-            printf("O triângulo é equilátero.\n");
+            printf("O triângulo é equilátero.");
         } else if (a == b || a == c || b == c) {
-            printf("O triângulo é isósceles.\n");
+            printf("O triângulo é isósceles.");
         } else {
-            printf("O triângulo é escaleno.\n");
+            printf("O triângulo é escaleno.");
         }
     } else {
         printf("Os lados  não formam um triângulo.\n");
