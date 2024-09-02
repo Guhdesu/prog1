5e6f63cf7ff11a8300ca037f7a13c4e160a8f783
Gustavo Ferreira <carvalhogustavo444@gmail.com>
2024-09-02 19:36:38 -0300

    Create questao03.c

---
+++ Atividade 03/questao03.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+	int N;
+
+	printf("Digite o valor de N: ");
+	scanf("%d", &N);
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N - i; j++) {
+			printf(" ");
+		}
+
+		for (int j = 1; j <= i; j++) {
+			printf(" X");
+		}
+		
+		
+		printf("\n");
+	}
+	
+	for (int i = N-1; i >= 1; i--) {
+		for (int j = 1; j <= N - i; j++) {
+			printf(" ");
+		}
+
+		for (int j = 1; j <= i; j++) {
+			printf(" X");
+		}
+		printf("\n");
+	    
+	}
+
+	return 0;
+}
