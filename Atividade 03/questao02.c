5defad415a405c29e41413ce4d7a69049f2f8c88
Gustavo Ferreira <carvalhogustavo444@gmail.com>
2024-09-02 19:22:21 -0300

    Create questao02.c

---
+++ Atividade 03/questao02.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+
+    do {
+        printf("Digite o valor de N (deve ser >= 2): ");
+        scanf("%d", &N);
+    } while (N < 2);
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N - i; j++) {
+            printf("   ");
+        }
+
+        for (int j = 1; j <= i; j++) {
+            printf(" %d ", j);
+        }
+
+        for (int j = i - 1; j >= 1; j--) {
+            printf(" %d ", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
